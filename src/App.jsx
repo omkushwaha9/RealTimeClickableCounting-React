260a11fb04ac70945fd1aa872cc78904e8883a00
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-28 13:15:42 +0530

    Minimal ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { useState } from 'react'
 
 function App() {
-  var [a,b] = useState(0);
+  var [a, b] = useState(0);
   return (
-    <div className=' w-full h-screen bg-lime-300'>
-     <h1 className='bg-pink-400 text-2xl font-mono p-4 text-center'> Basic RealTime Calculations </h1>
+    <div className=' w-full h-screen bg-purple-100'>
+      <h1 className='bg-pink-200 text-2xl font-mono p-4 text-center'> RealTime Clickable Counting  </h1>
 
-     <div className='w-60 h-72 bg-yellow-300 rounded-3xl items-center'>
-      <h1 className='text-center p-10' >{a}</h1>
-      <button
-      onClick={()=>b(a+1)}
-      className='w-20 h-10 bg-purple-500 rounded-xl text-zinc-100'
-      >
-        Count
-      </button>
-     </div>
+      <div className=' mt-40 ml-[40%] flex justify-center  h-12 w-64 bg-red-100 rounded-3xl items-center'>
+        <h1 className=' text-center p-10 text-4xl ' >{a}</h1>
+        <button
+          onClick={() => b(a + 1)}
+          className='w-20 h-10 bg-emerald-900 rounded-2xl text-zinc-100'
+        >
+          <h1 className='text-xl font-bold font-mono'>
+            Count
+          </h1>
+        </button>
+      </div>
     </div>
   )
 }
