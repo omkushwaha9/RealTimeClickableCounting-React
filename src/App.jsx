63b956af446e4b222797a67f0f1ea6e649377b37
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-28 10:36:41 +0530

    BG Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div className=' bg-sky-400'>
-     <h1 className='bg-purple-500 text-2xl font-mono p-12 text-center'> Notch </h1>
+    <div className=' w-full h-screen bg-lime-300'>
+     <h1 className='bg-pink-400 text-2xl font-mono p-12 text-center'> Notch </h1>
     </div>
   )
 }
