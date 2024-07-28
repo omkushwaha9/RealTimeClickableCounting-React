7536d1f25fc2b81f4a6fac4b3fba97a3033352f6
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-28 11:19:51 +0530

    Count Box

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className=' w-full h-screen bg-lime-300'>
      <h1 className='bg-pink-400 text-2xl font-mono p-4 text-center'> Basic RealTime Calculations </h1>
 
-     <div className='w-60 h-72 bg-yellow-300 rounded-3xl '>
+     <div className='w-60 h-72 bg-yellow-300 rounded-3xl items-center'>
       <h1 className='text-center p-10' >{a}</h1>
       <button
       onClick={()=>b(a+1)}
